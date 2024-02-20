cea42ddf8d1462e34b3dda09ac4006bce20e3321
edwardms <simeanu.eduard@gmail.com>
2024-02-20 15:53:21 +0200

    first commit, level 1 security - register users with username and password

---
+++ app.js
@@ -0,0 +1,90 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const ejs = require('ejs');
+const mongoose = require('mongoose');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const userSchema = mongoose.Schema(
+    {
+        email: {
+            type: String,
+            require: true
+        },
+        password: {
+            type: String,
+            require: true
+        }
+    }
+);
+
+const User = new mongoose.model('user', userSchema);
+
+app.get('/', (req, res) => {
+    res.render('home.ejs');
+});
+
+app.route('/register')
+    .get( (req, res) => {
+        res.render('register.ejs');
+    })
+    .post( (req, res) => {
+        const newUser = new User(
+            {
+                email: req.body.username,
+                password: req.body.password
+            }
+        );
+
+        newUser.save()
+            .then( () => {
+                console.log('User saved');
+                res.render('secrets.ejs')
+            })
+            .catch( err => {
+                console.log(err);
+            });
+    })
+
+app.route('/login')
+    .get( (req, res) => {
+        res.render('login.ejs');        
+    })
+    .post( (req, res) => {
+        const username = req.body.username;
+        const password = req.body.password;
+
+        User.findOne({email: username})
+            .then( foundUser => {
+                if (foundUser) {
+                    if (foundUser.password === password) res.render('secrets.ejs');
+                    else res.redirect('/login');
+                }
+                else res.redirect('/login');          
+            })
+            .catch( err => {
+                console.log(err);
+            });
+    });
+    
+/*
+get'/'
+get+post'/login'
+get+post'/register'
+get+post'/submit'
+*/
+
+mongoose.connect('mongodb://127.0.0.1:27017/userDB')
+    .then( () => {
+        console.log('MongoDB connected');
+        app.listen(3000, () => {
+            console.log('Server started on port 3000')
+        });
+    })
+    .catch( err => {
+        console.log(err);
+    });
